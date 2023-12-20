e58403bf99456d8dfc71515d84df146154197bac
Asat1llo <asatilloiminjonov57@gmail.com>
2023-12-21 00:08:04 +0500

    push changes

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 const elTodotext = document.querySelector('.todo__container__text')
 const elContainer = document.querySelector('.todo__list')
 
-let array = []
 
 function onClick1() {
     event.preventDefault()
     const elValue = elTodotext.value
-    array.push(elValue)
-    onMap()
-}
+    if (elValue == "") {
+        elTodotext.classList.add('active__input')
+    }
+    else {
+        const li = document.createElement('li')
+        const p = document.createElement('p')
+        const delet = document.createElement('button')
+        elTodotext.classList.remove('active__input')
 
-function onMap() {
-    event.preventDefault()
-    array.forEach((items, index) => {
-        onCreat(items)
-    });
-}
+        li.classList.add('todo__link')
+        delet.classList.add('todo__delete')
+        p.classList.add('text')
 
-
-function onCreat(items) {
-    const valuetext = document.createElement('li')
-    valuetext.classList.add('text')
-    elContainer.append(valuetext)   
+        elContainer.append(li)
+        li.append(p)
+        li.append(delet)
+        p.innerHTML = elValue
+    }
     
-    if (items == "." || items == null) {
-        console.log('error');
-    }
-    else  {
-        valuetext.innerHTML = items
-        elTodotext.value = null
-        array = []
-    }
+    elTodotext.value = ""
 }
 
+elContainer.addEventListener('click',function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle('active')
+    }
+    else if (e.target.classList == "todo__delete"){
+        e.target.parentElement.remove()
+    }
+},false)
 
